731cde4fa6050401518ebf650ba829c2d7b41551
LigeirinhoNaCama <mhlg07@gmail.com>
2025-02-26 21:41:53 -0300

    codigos da aula01

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,10 @@
+/*
+  Estrutura basica de um programa em C
+*/
+#include <stdio.h> // inclusão de bibliotecas
+
+// funcao principal que inicia o programa
+int main() {
+    printf("Ola pessoal!\n");
+    return 0;
+}
